0f091f89612e2b1fba66739c26dbae3c88ec4bd9
Mikalangelo Wessel <mwessel27@gmail.com>
2020-09-27 11:43:03 -0400

    Dynamic shelf view that looks much better. Need to get rid of the table view and allow for deep linking and showing new VC instead.

---
+++ bobble/bobble/MessageView.swift
@@ -0,0 +1,21 @@
+//
+//  MessageView.swift
+//  bobble
+//
+//  Created by Mikalangelo Wessel on 9/17/20.
+//  Copyright © 2020 Mikalangelo Wessel. All rights reserved.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageView()
+    }
+}
